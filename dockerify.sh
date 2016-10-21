f309287f1736945ea94ba7fe5abcc1a5a4a3c754
Ben Gummer <bgummer@atlassian.com>
2016-10-21 12:28:51 +1100

    chore: various build updates

--- dockerify.sh
+++ dockerify.sh
@@ -16,6 +16,8 @@ docker push "$NEW_IMAGE"
 # write new version to micros service descriptor
 sed -i .bak "s/tag: .*/tag: v${NEW_VERSION}/" frogmarch.sd.yml
 rm -f frogmarch.sd.yml.bak
+git add frogmarch.sd.yml
+git commit -m "chore: updating service descriptor to v$NEW_VERSION"
 
 # publish to micros in fast mode
 sleep 10
