420db24145c28482c56be99ecda9bf19acd31462
Ben Gummer <bgummer@atlassian.com>
2017-02-01 09:17:49 -0600

    chore: remove console.log

--- src/util/yarn.js
+++ src/util/yarn.js
@@ -49,7 +49,6 @@ function doYarn(buildDir) {
 
 // entry fn
 function yarnInstall(packages) {
-  console.log('YARN', { packages });
   return new Promise((resolve, reject) => {
     tmpDir().then(createPkgJsonFile.bind(null, packages)).then(doYarn).then((pkgJsonResults) => {
       const { buildDir } = pkgJsonResults;
