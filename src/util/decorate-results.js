56648370d1c349c5ef37024d7c9a0e2c3969e98c
Ben Gummer <bgummer@atlassian.com>
2017-10-12 11:39:59 -0700

    EOL notice

--- src/util/decorate-results.js
+++ src/util/decorate-results.js
@@ -1,9 +1,18 @@
 // @flow
 
+const deprecationMsg = [
+  'END OF LIFE NOTICE: PKGZIP SERVICE IS DEPRECATED AND WILL STOP WORKING ON JANUARY 1 2018.',
+  'THANK YOU FOR USING PKGZIP, BUT WE ARE SWITCHING IT OFF AS THERE ARE MUCH BETTER TOOLS',
+  'OUT THERE NOW, SUCH AS THE AWESOME CODESANDBOX.IO',
+].join(' ');
+
 function decorateResults(allPkgNames: Array<string>, resultsBlob: string) {
   const helpText = allPkgNames.map(pkg => `\n// window.pkgzip['${pkg}']`).join('');
 
-  return resultsBlob && `// The following objects are now available!:
+  return resultsBlob && `
+console.warn(${JSON.stringify(deprecationMsg)});
+
+// The following objects are now available!:
 ${helpText}
 
 // ============
