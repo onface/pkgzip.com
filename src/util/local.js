eb37c53db88c0c6bd3305f80b82a1e89461eff15
Nimo <nimo.jser@gmail.com>
2018-05-12 20:55:41 +0800

    cache object

--- src/util/local.js
+++ src/util/local.js
@@ -1,43 +1,22 @@
 var fs = require('fs')
 var path = require('path')
+var cache = {}
 module.exports = {
     upload: function (filename, content) {
         return new Promise(function (resolve, reject) {
-            fs.writeFile(
-                path.join(__dirname, '../../_cache/', filename),
-                content,
-                function (err) {
-                    if (err) {
-                        reject(err.message)
-                    }
-                    else {
-                        resolve()
-                    }
-                }
-            )
+            cache[filename] = content
+            resolve()
         })
     },
     download: function (filename) {
         return new Promise(function (resolve, reject) {
-            reject()
-            // fs.exists(filename, function (exists) {
-            //     if (!exists) {
-            //         reject()
-            //     }
-            //     else {
-            //         fs.readFile(
-            //             path.join(__dirname, '../../_cache/', filename),
-            //             function (err, content) {
-            //                 if (err) {
-            //                     reject(err.message)
-            //                 }
-            //                 else {
-            //                     resolve(content.toString())
-            //                 }
-            //             }
-            //         )
-            //     }
-            // })
+            if (typeof cache[filename] === 'undefined') {
+                resolve()
+            }
+            else {
+                resolve(cache[filename])
+            }
+
         })
     }
 }
